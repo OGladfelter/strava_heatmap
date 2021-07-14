6482b9467523b85092408c81383351bf2bbebbf9
ogladfelter <ogladfelter@gmail.com>
2021-07-14 00:58:26 -0500

    get changes ready for release

--- heatmap-no-API/js/ui.js
+++ heatmap-no-API/js/ui.js
@@ -98,18 +98,18 @@ function showActivityMenu() {
 }
   
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
 
 // function reverse_geocode_no_php(latlng){
 
